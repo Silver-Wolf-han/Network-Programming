d7415f3372fc2a47d6681df7924d89d0a7db4182
Silver-Wolf-han <zjinghan.cs12@nycu.edu.tw>
2025-04-08 10:13:14 +0800

    Copy Project1 and re-organize working directory

---
+++ Makefile
@@ -0,0 +1,33 @@
+CC	= gcc
+CXX	= g++
+CFLAGS	= -Wall -g
+MAKE	= make
+DIRS	= cmd
+
+PROGS	= np_sample np_sample_proc
+
+all: $(PROGS)
+
+%: %.cpp
+	$(CXX) -o $@ $(CFLAGS) $<
+
+run_test: $(PROGS)
+	mkdir working_directory
+	mkdir working_directory/bin
+	cd ..
+	for d in $(DIRS); do $(MAKE) -C $$d || exit 1; done
+	cp ./cmd/noop ./working_directory/bin/noop
+	cp ./cmd/number ./working_directory/bin/number
+	cp ./cmd/removetag ./working_directory/bin/removetag
+	cp ./cmd/removetag0 ./working_directory/bin/removetag0
+	cp ./cmd/manyblessings ./working_directory/bin/manyblessings
+	cp ./test.html ./working_directory/test.html
+	cp /usr/bin/ls ./working_directory/bin/ls 
+	cp /usr/bin/cat ./working_directory/bin/cat
+	cp /usr/bin/wc ./working_directory/bin/wc
+
+clean:
+	rm -f *~ $(PROGS)
+	for d in $(DIRS); do $(MAKE) -C $$d clean; done
+	rm -rf working_directory
+
---
+++ np_sample.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <sys/wait.h>   // waitpid()
+#include <fcntl.h>      // open()
+#include <unistd.h>     // getcwd(), STDIN_FILENO, STDOUT_FILENO
+#include <pwd.h>        // getpwuid()
+#include <signal.h>     // signal()
+
+constexpr int MAX_SIZE = 1000;
+
+constexpr int END_OF_COMMAND = 0;   // op
+constexpr int PIPE = 1;
+constexpr int NUM_PIPE = 2;
+constexpr int NUM_PIPE_ERR = 3;
+constexpr int OUT_RD = 4;
+// constexpr int IGNORE = 5;
+
+constexpr int NOT_PIPE = -1;        // opOrder
+constexpr int NOT_NUMBER_PIPE = 1;
+
+using namespace std;
+
+struct pipeStruct {
+    int OutCommandIndex;        // pipe output to which command
+    int fd[2];                  // pipe
+    vector<pid_t> relate_pids;  // input proc and previous proc
+    int startCommandType;       // record the start command type
+};
+
+struct Info {
+    bool bg;                        // Execute in the background
+    vector<int> op;                 // 0: single process, 1: pipline, 2: output redirection
+    vector<int> opOrder;            // output to which command
+    vector<vector<string>> argv;    // Arguments for first and second operand
+};
+
+void sigchld_handler(int signo);
+void typePrompt(bool showPath);
+int builtInCommand(Info info);
+int readCommand(Info &info, const int totalCommandCount);
+void executeCommand(Info info, map<int, struct pipeStruct>& pipeMap, const int currentCommandStart, const int totalCommandCount);
