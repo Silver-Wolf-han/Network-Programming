32e7516c92fffd439a2667429407c3d9b825ddb4
Silver-Wolf-han <zjinghan.cs12@nycu.edu.tw>
2025-04-12 09:48:51 +0800

    Reconstruct SingleProcessServer

--- np_sample_proc.cpp
+++ np_sample_proc.cpp
@@ -40,6 +40,199 @@ void singleProcessConcurrentServer(int port) {
         exit(1);
     }
 
+    // server addr test
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    memset(&(server_addr.sin_zero), 0, 8);
+
+    // bind
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+        cerr << "bind error" << endl;
+        exit(1);
+    }
+
+    // listen
+    if (listen(server_fd, MAX_CLIENT) == -1) {
+        cerr << "listen error" << endl;
+        exit(1);
+    }
+
+    // zombie processes
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        cerr << "sigaction error" << endl;
+        exit(1);
+    }
+    signal(SIGCHLD, sigchld_handler);
+
+    // FD handle
+    /* 1. Init client_fd_table */
+    for (size_t i = 0; i < MAX_CLIENT; ++i) {
+        client_fd_table[i] = FD_UNDEFINED;
+    }
+
+    /* 2. Init all_fd_set: managment all fd */
+    fd_set all_fd_set;
+    FD_ZERO(&all_fd_set);
+    FD_SET(server_fd, &all_fd_set);
+    
+    // man by select, so it should be int type rather than size_t
+    int cur_fd_size = 0;
+    // record max size in fd table
+    size_t max_fd_in_table = 0;
+    // fd variable
+    int max_fd_history = 50, connect_fd = FD_UNDEFINED;
+
+    /* 3. temp variable rset */
+    fd_set rset;
+
+    User_Info_Map.clear();
+    
+    dup2(STDIN_FILENO, 4);
+    dup2(STDOUT_FILENO, 5);
+    dup2(STDERR_FILENO, 6);
+
+    cout << "Server is listening on port:" << port << "..." << endl;
+
+    while (true) {
+        rset = all_fd_set;
+
+        // select: management fd
+        if ((cur_fd_size = select(max_fd_history+1, &rset, NULL, NULL, (struct timeval *)0)) == FD_UNDEFINED) {
+            if (errno == EINTR)
+                continue;
+            cerr << "select error" << endl;
+            continue;
+        }
+        if (FD_ISSET(server_fd, &rset)) {
+            // accept
+            sin_size = sizeof(client_addr);
+            if ((connect_fd = accept(server_fd, (struct sockaddr *)&client_addr, &sin_size)) == FD_UNDEFINED) {
+                cout << "accept error" << endl;
+                continue;
+            }
+
+            // storge fd from accept (in local)
+            size_t push_idx = 0;
+            for (size_t i = 1; i < MAX_CLIENT; ++i) {
+                if (client_fd_table[i] < 0) {
+                    client_fd_table[i] = connect_fd;
+                    push_idx = i;
+                    break;
+                }
+            }
+            if (push_idx == 0) {
+                cerr << "size of client too much" << endl;
+                exit(1);
+            }
+
+            // Init User_Info_Map
+            inet_ntop(AF_INET, &(client_addr.sin_addr), User_Info_Map[(int)push_idx].IPAddress,INET_ADDRSTRLEN);
+            User_Info_Map[(int)push_idx].port = ntohs(client_addr.sin_port);
+            User_Info_Map[(int)push_idx].UserName = "(no name)";
+            User_Info_Map[(int)push_idx].EnvVar["PATH"] = "bin:.";
+            User_Info_Map[(int)push_idx].totalCommandCount = 0;
+            User_Info_Map[(int)push_idx].pipeMap;
+
+            // Enveriment Variable Init
+            clearenv();
+            for(auto env : User_Info_Map[(int)push_idx].EnvVar){
+                setenv(env.first.c_str(), env.second.c_str(), 1);
+            }
+
+            // Welcome Mesage
+            dup2Client(client_fd_table[(int)push_idx]);
+            welcomeMsg();
+            string welmsg = "*** User '" + User_Info_Map[(int)push_idx].UserName + "' entered from " + User_Info_Map[(int)push_idx].IPAddress + ". ***";
+            broadcast(welmsg);
+            dup2Client(client_fd_table[(int)push_idx]);
+
+
+            // storge fd from accept (in FD_SET)
+            FD_SET(connect_fd, &all_fd_set);
+
+            // update fd num in history
+			if(connect_fd > max_fd_history) {
+                max_fd_history = connect_fd;
+            }
+
+            // update current fd max
+			if(push_idx > max_fd_in_table) {
+                max_fd_in_table = push_idx;
+            }
+
+            // if no one from accept then
+            if (--cur_fd_size <= 0) {
+                continue;
+            }
+            //--cur_fd_size;
+        }
+
+        for (size_t i = 1; i <= max_fd_in_table; ++i) {
+            if (client_fd_table[i] < 0) {
+                continue;
+            }
+
+            if (FD_ISSET(client_fd_table[i], &rset)) {
+                clearenv();
+                for(auto env : User_Info_Map[i].EnvVar){
+                    setenv(env.first.c_str(), env.second.c_str(), 1);
+                }
+
+                dup2Client(client_fd_table[i]);
+                bool exit = false;
+                User_Info_Map[i].pipeMap = npshell_handle_one_line(User_Info_Map[i].pipeMap, &exit, &User_Info_Map[i].totalCommandCount);
+                if (exit) {
+                    dup2(4, STDIN_FILENO);
+                    dup2(5, STDOUT_FILENO);
+                    dup2(6, STDERR_FILENO);
+                    close(client_fd_table[i]);
+                    FD_CLR(client_fd_table[i], &all_fd_set);
+                    client_fd_table[i] = -1;
+                    string logout_msg = "*** User '" + User_Info_Map[i].UserName + "' left. ***";
+                    broadcast(logout_msg);
+                    User_Info_Map.erase(i);
+                }
+                dup2(4, STDIN_FILENO);
+                dup2(5, STDOUT_FILENO);
+                dup2(6, STDERR_FILENO);
+                if (--cur_fd_size <= 0) {
+                    break;
+                }
+                //--cur_fd_size;
+            }
+        }
+    }
+}
+
+/*
+void singleProcessConcurrentServer(int port) {
+    int server_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+
+    // create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        cerr << "server socket error" << endl;
+        exit(1);
+    }
+
+    // set socket opt: address user test
+    int yes = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        cerr << "set sockopt address error" << endl;
+        exit(1);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &yes, sizeof(int)) == -1) {
+        cerr << "set sockopt port error" << endl;
+        exit(1);
+    }
+
     // bind
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -128,7 +321,6 @@ void singleProcessConcurrentServer(int port) {
             User_Info_Map[i].pipeMap;
             // pipe handler ????
 
-            /* Env var init */
             clearenv();
             for(auto env : User_Info_Map[i].EnvVar){
                 setenv(env.first.c_str(), env.second.c_str(), 1);
@@ -140,7 +332,6 @@ void singleProcessConcurrentServer(int port) {
             broadcast(welmsg);
             dup2Client(client_fd_table[i]);
             
-            /* diff between connect_fd and client id*/
             FD_SET(connect_fd, &all_fd_set);
 			if(connect_fd > MAX_FD_NUM) {
                 MAX_FD_NUM = connect_fd;
@@ -189,6 +380,7 @@ void singleProcessConcurrentServer(int port) {
         }
     }
 }
+*/
 
 void dup2Client(int fd) {
     dup2(fd, STDIN_FILENO);
